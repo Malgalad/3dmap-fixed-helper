340c019627a37d4da26de840bdb504de285b99cb
Ilya Pogorelov <Ilya.Pogorelov@contractor.cengage.com>
2026-04-04 01:31:02 +0300

    Use HTTP; v1 API

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,49 @@
 const express = require('express');
 const router = express.Router();
 
+const map = new Map();
+const addMarker = (clientId, position) => {
+  const stack = map.get(clientId) || [];
+  stack.push(position);
+  map.set(clientId, stack);
+}
+const getMarkers = (clientId) => map.get(clientId) || [];
+const clearMarkers = (clientId) => map.delete(clientId);
+
 router.get('/status', function(req, res, next) {
   res.send('OK');
 });
 
 router.get('/addmarker', function(req, res, next) {
-  const { pos } = req.query;
+  const { id, pos } = req.query;
+  const position = pos.split('+').map(parseFloat);
+  addMarker(id, position);
   res.sendStatus(204);
 })
 
-router.get('/map', function(req, res, next) {
-  res.sendStatus(204);
+router.get('/connect', function(req, res, next) {
+  const { id } = req.query;
+
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  res.write(`event: connect\n`);
+  res.write(`data: OK\n\n`);
+
+  const intervalId = setInterval(() => {
+    const markers = getMarkers(id);
+    if (markers.length > 0) {
+      res.write(`event: addMarker\n`);
+      res.write(`data: ${JSON.stringify(markers)}\n\n`);
+      clearMarkers(id);
+    }
+  }, 500);
+
+  req.on('close', () => {
+    clearInterval(intervalId);
+    res.end();
+  });
 });
 
 module.exports = router;
